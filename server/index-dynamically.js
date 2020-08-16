4a5c8cc867e5a69737c96d3cb54f41744e0069ad
a1824jj <a1824jj@aiit.ac.jp>
2020-08-16 12:25:20 +0000

    Statically or Dynamically CodeGeneration gRPC nodejs

---
+++ server/index-dynamically.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const protoLoader = require('@grpc/proto-loader')
+const grpc = require('grpc')
+
+//grpc service definition gor greet
+
+const greetProtoPath = path.join(__dirname, "..", "protos", "greet.proto")
+const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+
+const greetPackageDefinition = grpc.loadPackageDefinition(greetProtoDefinition).greet
