f7c7bb2a8acdb4948f8ccfe9980c2c8fcde83847
a1824jj <a1824jj@aiit.ac.jp>
2020-08-16 11:11:59 +0000

    Unary API Server Implementation

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
+var greets = require('../server/protos/greet_pb')
+var service = require('../server/protos/greet_grpc_pb')
+
 var grpc = require('grpc')
+
+/*
+  Implements thegreet RPC method.
+*/
+
+function greet(call, callback) {
+  var greeting = new greets.GreetResponse()
+  greeting.setResult(
+    "Hello " + call.request.getGreeting().getFirstname()
+  )
+  callback(null, greeting)
+}
 
 function main() {
   var server = new grpc.Server()
-  
+  server.addService(service.GreetServiceService, {greet: greet})
   server.bind("0.0.0.0:5000", grpc.ServerCredentials.createInsecure())
   server.start()
 
